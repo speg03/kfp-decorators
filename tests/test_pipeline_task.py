5a0d335b7397bcd34b1b4bcbe77bb6a0a17d5f57
Takahiro Yano <speg03@gmail.com>
2023-10-25 00:30:03 +0900

    Add test cases and settings to achieve 100% coverage
    Exclude auto-generated _version.py from coverage.

--- tests/test_pipeline_task.py
+++ tests/test_pipeline_task.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 
+import pytest
 import yaml
 from kfp import compiler
 from kfp_decorators import chain, cpu_request, dsl, memory_request
@@ -9,6 +10,7 @@ from kfp_decorators.types import BaseComponent, ComponentDecorator
 
 
 class TestCustomizedComponent:
+    @pytest.fixture
     def customized_component(self) -> BaseComponent:
         def customize_decorator() -> ComponentDecorator:
             def _decorator(fn: BaseComponent) -> BaseComponent:
@@ -23,6 +25,7 @@ class TestCustomizedComponent:
 
         return hello
 
+    @pytest.fixture
     def origin_component(self) -> BaseComponent:
         @dsl.component()
         def hello(message: str) -> str:
@@ -30,20 +33,75 @@ class TestCustomizedComponent:
 
         return hello
 
-    def test_compile_components(self, tmp_path: pathlib.Path):
-        customized_component = self.customized_component()
+    @pytest.fixture
+    def customized_pipeline(self) -> BaseComponent:
+        def customize_decorator() -> ComponentDecorator:
+            def _decorator(fn: BaseComponent) -> BaseComponent:
+                return CustomizedComponent(fn)
+
+            return _decorator
+
+        @customize_decorator()
+        @dsl.component()
+        def hello(message: str) -> str:
+            return message
+
+        @dsl.pipeline(name="hello")
+        def hello_pipeline(message: str = "hello") -> None:
+            hello(message=message)
+
+        return hello_pipeline
+
+    @pytest.fixture
+    def origin_pipeline(self) -> BaseComponent:
+        @dsl.component()
+        def hello(message: str) -> str:
+            return message
+
+        @dsl.pipeline(name="hello")
+        def hello_pipeline(message: str = "hello") -> None:
+            hello(message=message)
+
+        return hello_pipeline
+
+    def test_compile_components(
+        self,
+        tmp_path: pathlib.Path,
+        customized_component: BaseComponent,
+        origin_component: BaseComponent,
+    ):
         package_path = os.fspath(tmp_path / "customized_component.yaml")
         compiler.Compiler().compile(customized_component, package_path)
         with open(package_path, "r") as f:
             compiled_customized_component = yaml.safe_load(f)
 
-        origin_component = self.origin_component()
         package_path = os.fspath(tmp_path / "origin_component.yaml")
         compiler.Compiler().compile(origin_component, package_path)
         with open(package_path, "r") as f:
             compiled_origin_component = yaml.safe_load(f)
 
         assert compiled_customized_component == compiled_origin_component
+
+    def test_compile_pipelines(
+        self,
+        tmp_path: pathlib.Path,
+        customized_pipeline: BaseComponent,
+        origin_pipeline: BaseComponent,
+    ):
+        package_path = os.fspath(tmp_path / "customized_pipeline.yaml")
+        compiler.Compiler().compile(customized_pipeline, package_path)
+        with open(package_path, "r") as f:
+            compiled_customized_pipeline = yaml.safe_load(f)
+
+        package_path = os.fspath(tmp_path / "origin_pipeline.yaml")
+        compiler.Compiler().compile(origin_pipeline, package_path)
+        with open(package_path, "r") as f:
+            compiled_origin_pipeline = yaml.safe_load(f)
+
+        assert compiled_customized_pipeline == compiled_origin_pipeline
+
+    def test_local_execute(self, customized_component: BaseComponent):
+        assert customized_component.execute(message="hello") == "hello"
 
 
 class TestChain:
